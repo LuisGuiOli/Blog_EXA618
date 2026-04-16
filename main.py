51388ca0020efa9d12429b508cebd999ba2c9943
Luis Guilherme <guigonl16@gmail.com>
2026-04-15 21:34:30 -0300

    Finalizando projeto

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import os
-
 import redis
 import json
 from fastapi import FastAPI
+from pydantic import BaseModel 
 import datetime
 
 app = FastAPI()
-REDIS_URL = os.getenv("KV_URL")
 
+# Defina a estrutura que a API espera receber
+class Mensagem(BaseModel):
+    author: str
+    message: str
+
+REDIS_URL = os.getenv("KV_URL")
 r = redis.Redis.from_url(REDIS_URL, decode_responses=True)
 
 @app.post("/blog/registrar")
-def registrar(author: str, message: str):
-    
+def registrar(item: Mensagem): # Use o modelo aqui
     nova_mensagem = {
-        "author": author,
-        "message": message,
+        "author": item.author,
+        "message": item.message,
         "timestamp": str(datetime.datetime.now())
     }
     
@@ -24,6 +28,5 @@ def registrar(author: str, message: str):
 
 @app.get("/blog/mensagens")
 def listar():
-    
     mensagens = r.lrange("mensagens_blog", 0, -1)
     return [json.loads(m) for m in mensagens]
