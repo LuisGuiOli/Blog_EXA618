a01f4883e7f3bec7c6fbe20ea278dfa58b051aa4
Luis Guilherme <guigonl16@gmail.com>
2026-04-15 23:03:25 -0300

    Finalizando projeto

--- api/index.py
+++ api/index.py
@@ -1,30 +1,11 @@
-import os
-import json
-import redis
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-import datetime
 
 app = FastAPI()
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+@app.get("/api")
+def hello_world():
+    return {"message": "Olá, API Python na Vercel!"}
 
-# REDIS_URL = os.getenv("KV_URL")
-
-# r = redis.Redis.from_url(REDIS_URL, decode_responses=True)
-
-
-@app.post("/api")
-def enviar_mensagem():
-    return {"status": "Mensagem salva no Redis!"}
-
-
-# @app.get("/mensagens")
-# def listar():
-#     mensagens_brutas = r.lrange("mensagens_blog", 0, -1)
-#     return [json.loads(m) for m in mensagens_brutas]
+@app.get("/api/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
