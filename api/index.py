51579afacb247858455b9ddffd7d6189ee49baf3
Luis Guilherme <guigonl16@gmail.com>
2026-04-15 22:31:34 -0300

    Finalizando projeto

--- api/index.py
+++ api/index.py
@@ -19,7 +19,7 @@ REDIS_URL = os.getenv("REDIS_URL")
 r = redis.Redis.from_url(REDIS_URL, decode_responses=True)
 
 
-@app.post("/blog/registrar")
+@app.post("/api/blog/registrar")
 def registrar(author: str, message: str):
     nova_mensagem = {
         "author": author,
@@ -31,7 +31,7 @@ def registrar(author: str, message: str):
     
     return {"status": "Mensagem salva no Redis!"}
 
-@app.get("/blog/mensagens")
+@app.get("/api/blog/mensagens")
 def listar():
     mensagens_brutas = r.lrange("mensagens_blog", 0, -1)
     return [json.loads(m) for m in mensagens_brutas]
