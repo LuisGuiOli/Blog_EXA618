409294729bbe49e036e1b1d3a6d41a931809a847
Luis Guilherme <guigonl16@gmail.com>
2026-04-15 22:22:54 -0300

    Finalizando projeto

--- api/index.py
+++ api/index.py
@@ -2,11 +2,19 @@ import os
 import json
 import redis
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import datetime
 
 app = FastAPI()
 
-REDIS_URL = os.getenv("KV_URL")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+REDIS_URL = os.getenv("REDIS_URL")
 
 r = redis.Redis.from_url(REDIS_URL, decode_responses=True)
 
@@ -22,3 +30,8 @@ def registrar(author: str, message: str):
     r.lpush("mensagens_blog", json.dumps(nova_mensagem))
     
     return {"status": "Mensagem salva no Redis!"}
+
+@app.get("/blog/mensagens")
+def listar():
+    mensagens_brutas = r.lrange("mensagens_blog", 0, -1)
+    return [json.loads(m) for m in mensagens_brutas]
